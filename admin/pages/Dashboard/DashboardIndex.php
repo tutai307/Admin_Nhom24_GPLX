db080c880200bbec4e053bbad8961d0ee6470dc6
tutai307 <tutaihocgioi@gmail.com>
2024-03-31 20:43:29 +0700

    no mess

--- admin/pages/Dashboard/DashboardIndex.php
+++ admin/pages/Dashboard/DashboardIndex.php
@@ -614,6 +614,6 @@
         </div>
 
     </div>
-    <script src="http://maps.google.com/maps/api/js?sensor=true"></script>
+    <!-- <script src="http://maps.google.com/maps/api/js?sensor=true"></script> -->
     <script type="text/javascript" src="https://demo.dashboardpack.com/architectui-html-free/assets/scripts/main.js"></script>
 </div>
